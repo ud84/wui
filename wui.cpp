29c3c9ca447c43cfaef39dee2efa57f69803ff29
Anton Golovkov <udattsk@gmail.com>
2021-12-22 12:57:25 +0700

    Example improved

--- wui.cpp
+++ wui.cpp
@@ -7,6 +7,44 @@
 #include <WUI/Window/Window.h>
 #include <WUI/Control/Button.h>
 
+struct PluggedWindow
+{
+	std::shared_ptr<WUI::Window> &parentWindow;
+
+	std::shared_ptr<WUI::Window> window;
+	std::shared_ptr<WUI::Button> plugButton, unplugButton;
+
+	void Plug()
+	{
+		window->Destroy();
+
+		parentWindow->AddControl(window, WUI::Rect{ 50, 50, 250, 250 });
+
+		window->AddControl(unplugButton, WUI::Rect{ 10, 10, 110, 35 });
+		window->AddControl(plugButton, WUI::Rect{ 10, 55, 110, 80 });
+
+		plugButton->Disable();
+		unplugButton->Enable();
+	}
+
+	void Unplug()
+	{
+		parentWindow->RemoveControl(window);
+		window->Init(WUI::WindowType::Dialog, WUI::Rect{ 50, 50, 250, 250 }, L"Child window unplugged!", []() {});
+
+		plugButton->Enable();
+		unplugButton->Disable();
+	}
+
+	PluggedWindow(std::shared_ptr<WUI::Window> &parentWindow_)
+		: parentWindow(parentWindow_),
+		window(new WUI::Window()),
+		plugButton(new WUI::Button(L"Plug Window", std::bind(&PluggedWindow::Plug, this))),
+		unplugButton(new WUI::Button(L"Unplug Window", std::bind(&PluggedWindow::Unplug, this)))
+	{}
+};
+
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
@@ -18,19 +56,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	WUI::SetTheme(WUI::Theme::Dark);
 
 	std::shared_ptr<WUI::Window> window(new WUI::Window());
-	
-	std::shared_ptr<WUI::Window> parentWindow(new WUI::Window());
-	window->AddControl(parentWindow, WUI::Rect{ 50, 50, 250, 250 });
-	{
-		std::shared_ptr<WUI::Button> unplugButton(new WUI::Button(L"Unplug window", [window, parentWindow]() { window->RemoveControl(parentWindow); parentWindow->Init(WUI::WindowType::Dialog, WUI::Rect{ 50, 50, 250, 250 }, L"Child window unplugged!", []() {}); }));
-		std::shared_ptr<WUI::Button> plugButton(new WUI::Button(L"Plug window", [window, parentWindow]() { parentWindow->Destroy(); window->AddControl(parentWindow, WUI::Rect{ 50, 50, 250, 250 }); }));
-				
-		parentWindow->AddControl(unplugButton, WUI::Rect{ 10, 10, 110, 35 });
-		parentWindow->AddControl(plugButton, WUI::Rect{ 10, 55, 110, 80 });
-	}
 
-	std::shared_ptr<WUI::Button> darkThemeButton(new WUI::Button(L"Set the dark theme", [window, parentWindow]() { WUI::SetTheme(WUI::Theme::Dark); window->UpdateTheme(); parentWindow->UpdateTheme(); }));
-	std::shared_ptr<WUI::Button> whiteThemeButton(new WUI::Button(L"Set the white theme", [window, parentWindow]() { WUI::SetTheme(WUI::Theme::White); window->UpdateTheme(); parentWindow->UpdateTheme(); }));
+	PluggedWindow pluggedWindow(window);
+	pluggedWindow.Plug();
+
+	std::shared_ptr<WUI::Button> darkThemeButton(new WUI::Button(L"Set the dark theme", [window, pluggedWindow]() { WUI::SetTheme(WUI::Theme::Dark); window->UpdateTheme(); pluggedWindow.window->UpdateTheme(); }));
+	std::shared_ptr<WUI::Button> whiteThemeButton(new WUI::Button(L"Set the white theme", [window, pluggedWindow]() { WUI::SetTheme(WUI::Theme::White); window->UpdateTheme(); pluggedWindow.window->UpdateTheme(); }));
 
 	window->AddControl(darkThemeButton, WUI::Rect{ 140, 350, 150, 375 });
 	window->AddControl(whiteThemeButton, WUI::Rect{ 270, 350, 380, 375 });
