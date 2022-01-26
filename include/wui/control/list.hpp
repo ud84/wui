9312aaf5a4cb4af2aafff39f52222258ea43278d
Anton Golovkov <udattsk@gmail.com>
2022-01-26 16:59:07 +0700

    List control inited

---
+++ include/wui/control/list.hpp
@@ -0,0 +1,137 @@
+//
+// Copyright (c) 2021-2022 Anton Golovkov (udattsk at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/ud84/wui
+//
+
+#pragma once
+
+#include <wui/control/i_control.hpp>
+#include <wui/graphic/graphic.hpp>
+#include <wui/event/event.hpp>
+#include <wui/common/rect.hpp>
+#include <wui/common/color.hpp>
+
+#include <string>
+#include <vector>
+#include <functional>
+#include <memory>
+
+namespace wui
+{
+
+class list : public i_control, public std::enable_shared_from_this<list>
+{
+public:
+    list(std::shared_ptr<i_theme> theme_ = nullptr);
+    ~list();
+
+    virtual void draw(graphic &gr);
+
+    virtual void set_position(const rect &position);
+    virtual rect position() const;
+
+    virtual void set_parent(std::shared_ptr<window> window_);
+    virtual void clear_parent();
+
+    virtual bool topmost() const;
+
+    virtual void set_focus();
+    virtual bool remove_focus();
+    virtual bool focused() const;
+    virtual bool focusing() const;
+
+    virtual void update_theme(std::shared_ptr<i_theme> theme_ = nullptr);
+
+    virtual void show();
+    virtual void hide();
+    virtual bool showed() const;
+
+    virtual void enable();
+    virtual void disable();
+    virtual bool enabled() const;
+
+    /// list interface
+    void add_column(int32_t width, const std::string &caption);
+    
+    void select_item(int32_t n_item);
+    int32_t selected_item() const;
+
+    void set_column_width(int32_t n_item, int32_t width);
+    void set_item_height(int32_t height);
+    
+    void set_item_count(int32_t count);
+
+    void set_draw_callback(std::function<void(graphic&, int32_t, const rect&, bool selected)> draw_callback_);
+    void set_item_change_callback(std::function<void(int32_t)> item_change_callback_);
+    void set_column_click_callback(std::function<void(int32_t)> column_click_callback_);
+    void set_item_click_callback(std::function<void(int32_t)> item_click_callback_);
+    void set_item_double_click_callback(std::function<void(int32_t)> item_double_click_callback_);
+    void set_item_right_click_callback(std::function<void(int32_t)> item_right_click_callback_);
+    
+public:
+    /// Control name in theme
+    static constexpr const char *tc = "list";
+
+    /// Used theme values
+    static constexpr const char *tv_background = "background";
+    static constexpr const char *tv_border = "border";
+    static constexpr const char *tv_border_width = "border_width";
+    static constexpr const char *tv_title = "title";
+    static constexpr const char *tv_title_text = "title_text";
+    static constexpr const char *tv_scrollbar = "scrollbar";
+    static constexpr const char *tv_scrollbar_slider = "scrollbar_slider";
+    static constexpr const char *tv_scrollbar_arrow_left_shadow = "scrollbar_arrow_left_shadow";
+    static constexpr const char *tv_scrollbar_arrow_right_shadow = "scrollbar_arrow_right_shadow";
+    static constexpr const char *tv_selected = "selected";
+    static constexpr const char *tv_select_active = "select_active";
+    static constexpr const char *tv_round = "round";
+    static constexpr const char *tv_font = "font";
+
+private:
+    std::shared_ptr<i_theme> theme_;
+
+    rect position_;
+
+    std::weak_ptr<window> parent;
+
+    bool showed_, enabled_, focused_;
+
+    struct column
+    {
+        int32_t width;
+        std::string caption;
+    };
+    std::vector<column> columns;
+
+    int32_t item_height, item_count, selected_item_, start_item;
+
+    std::function<void(graphic&, int32_t, const rect&, bool selected)> draw_callback;
+    std::function<void(int32_t)> item_change_callback;
+    std::function<void(int32_t)> column_click_callback;
+    std::function<void(int32_t)> item_click_callback;
+    std::function<void(int32_t)> item_double_click_callback;
+    std::function<void(int32_t)> item_right_click_callback;
+    
+    void receive_event(const event &ev);
+
+    void redraw();
+
+    void draw_titles(graphic &gr_);
+
+    void draw_items(graphic &gr_);
+
+    void draw_scrollbar(graphic &gr_);
+
+    int32_t get_title_height() const;
+    int32_t get_visible_item_count() const;
+    
+    void scroll_up();
+    void scroll_down();
+};
+
+}
+
---
+++ src/control/list.cpp
@@ -0,0 +1,332 @@
+//
+// Copyright (c) 2021-2022 Anton Golovkov (udattsk at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/ud84/wui
+//
+
+#include <wui/control/list.hpp>
+
+#include <wui/window/window.hpp>
+
+#include <wui/theme/theme.hpp>
+
+#include <algorithm>
+
+namespace wui
+{
+
+list::list(std::shared_ptr<i_theme> theme__)
+    : theme_(theme__),
+    position_(),
+    parent(),
+    showed_(false), enabled_(false), focused_(false),
+    columns(),
+    item_height(0), item_count(0), selected_item_(0), start_item(0),
+    draw_callback(),
+    item_change_callback(),
+    column_click_callback(),
+    item_click_callback(),
+    item_double_click_callback(),
+    item_right_click_callback()
+{
+}
+
+list::~list()
+{
+    auto parent_ = parent.lock();
+    if (parent_)
+    {
+        parent_->remove_control(shared_from_this());
+    }
+}
+
+void list::draw(graphic &gr)
+{
+    if (!showed_)
+    {
+        return;
+    }
+
+    gr.draw_rect(position_,
+        theme_color(tc, tv_border, theme_),
+        theme_color(tc, tv_background, theme_),
+        theme_dimension(tc, tv_border_width, theme_),
+        theme_dimension(tc, tv_round, theme_));
+
+    draw_titles(gr);
+
+    draw_items(gr);
+
+    draw_scrollbar(gr);
+}
+
+void list::receive_event(const event &)
+{
+    if (!showed_ || !enabled_)
+    {
+        return;
+    }
+}
+
+void list::set_position(const rect &position__)
+{
+    auto prev_position = position_;
+    position_ = position__;
+
+    if (showed_)
+    {
+        auto parent_ = parent.lock();
+        if (parent_)
+        {
+            parent_->redraw(prev_position, true);
+        }
+    }
+	
+    redraw();
+}
+
+rect list::position() const
+{
+	return position_;
+}
+
+void list::set_parent(std::shared_ptr<window> window)
+{
+    parent = window;
+}
+
+void list::clear_parent()
+{
+    parent.reset();
+}
+
+bool list::topmost() const
+{
+    return false;
+}
+
+void list::set_focus()
+{
+    if (enabled_ && showed_)
+    {
+        focused_ = true;
+
+        redraw();
+    }
+}
+
+bool list::remove_focus()
+{
+    focused_ = false;
+
+    redraw();
+
+    return true;
+}
+
+bool list::focused() const
+{
+    return focused_;
+}
+
+bool list::focusing() const
+{
+    return enabled_ && showed_;
+}
+
+void list::update_theme(std::shared_ptr<i_theme> theme__)
+{
+    if (theme_ && !theme__)
+    {
+        return;
+    }
+    theme_ = theme__;
+
+    redraw();
+}
+
+void list::show()
+{
+    if (!showed_)
+    {
+        showed_ = true;
+        redraw();
+    }
+}
+
+void list::hide()
+{
+    if (showed_)
+    {
+        showed_ = false;
+        auto parent_ = parent.lock();
+        if (parent_)
+        {
+            parent_->redraw(position_, true);
+        }
+    }
+}
+
+bool list::showed() const
+{
+    return showed_;
+}
+
+void list::enable()
+{
+    enabled_ = true;
+    redraw();
+}
+
+void list::disable()
+{
+    enabled_ = false;
+    redraw();
+}
+
+bool list::enabled() const
+{
+    return enabled_;
+}
+
+void list::add_column(int32_t width, const std::string &caption)
+{
+    columns.emplace_back(column{ width, caption });
+}
+
+void list::select_item(int32_t n_item)
+{
+    selected_item_ = n_item;
+
+    auto visible_item_count = get_visible_item_count();
+
+    if (selected_item_ < start_item)
+    {
+        auto diff = start_item - selected_item_;
+        for (auto i = 0; i != diff; ++i)
+        {
+            scroll_up();
+        }
+    }
+    else if (selected_item_ > visible_item_count + start_item)
+    {
+        auto diff = selected_item_ + 1 - visible_item_count + start_item;
+        for (auto i = 0; i != diff; ++i)
+        {
+            scroll_down();
+        }
+    }
+
+    redraw();
+}
+
+int32_t list::selected_item() const
+{
+    return selected_item_;
+}
+
+void list::set_column_width(int32_t n_item, int32_t width)
+{
+    if (columns.size() > n_item)
+    {
+        columns[n_item].width = width;
+        redraw();
+    }
+}
+
+void list::set_item_height(int32_t height)
+{
+    item_height = height;
+}
+
+void list::set_item_count(int32_t count)
+{
+    item_count = count;
+    redraw();
+}
+
+void list::set_draw_callback(std::function<void(graphic&, int32_t, const rect&, bool selected)> draw_callback_)
+{
+    draw_callback = draw_callback_;
+}
+
+void list::set_item_change_callback(std::function<void(int32_t)> item_change_callback_)
+{
+    item_change_callback = item_change_callback_;
+}
+
+void list::set_column_click_callback(std::function<void(int32_t)> column_click_callback_)
+{
+    column_click_callback = column_click_callback_;
+}
+
+void list::set_item_click_callback(std::function<void(int32_t)> item_click_callback_)
+{
+    item_click_callback = item_click_callback_;
+}
+
+void list::set_item_double_click_callback(std::function<void(int32_t)> item_double_click_callback_)
+{
+    item_double_click_callback = item_double_click_callback_;
+}
+
+void list::set_item_right_click_callback(std::function<void(int32_t)> item_right_click_callback_)
+{
+    item_right_click_callback = item_right_click_callback_;
+}
+
+void list::redraw()
+{
+    if (showed_)
+    {
+        auto parent_ = parent.lock();
+        if (parent_)
+        {
+            parent_->redraw(position_);
+        }
+    }
+}
+
+int32_t list::get_title_height() const
+{
+    if (!columns.empty())
+    {
+        return 20;
+    }
+
+    return 0;
+}
+
+int32_t list::get_visible_item_count() const
+{
+    return static_cast<int32_t>(position_.height() / item_height);
+}
+
+void list::scroll_up()
+{
+    if (start_item == 0)
+    {
+        return;
+    }
+
+    --start_item;
+    redraw();
+}
+
+void list::scroll_down()
+{
+    auto visible_item_count = get_visible_item_count();
+
+    if (visible_item_count >= item_count || start_item == item_count - visible_item_count)
+    {
+        return;
+    }
+
+    ++start_item;
+    redraw();
+}
+
+}
