f5c1003b84ea68572576a17c88cef626fac94c74
Anton Golovkov <udattsk@gmail.com>
2022-02-13 14:23:41 +0700

    Splitter added

---
+++ include/wui/control/splitter.hpp
@@ -0,0 +1,91 @@
+//
+// Copyright (c) 2021-2022 Anton Golovkov (udattsk at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/ud84/wui
+//
+
+#pragma once
+
+#include <wui/control/i_control.hpp>
+#include <wui/graphic/graphic.hpp>
+#include <wui/event/event.hpp>
+#include <wui/common/rect.hpp>
+#include <wui/common/color.hpp>
+
+#include <functional>
+#include <memory>
+
+namespace wui
+{
+
+enum class splitter_orientation
+{
+    vertical,
+    horizontal
+};
+
+class splitter : public i_control, public std::enable_shared_from_this<splitter>
+{
+public:
+    splitter(splitter_orientation orientation, std::function<void(int32_t, int32_t)> callback, std::shared_ptr<i_theme> theme_ = nullptr);
+
+    ~splitter();
+
+    /// i_control impl
+    virtual void draw(graphic &gr, const rect &);
+
+    virtual void set_position(const rect &position, bool redraw = true);
+    virtual rect position() const;
+
+    virtual void set_parent(std::shared_ptr<window> window_);
+    virtual void clear_parent();
+
+    virtual bool topmost() const;
+
+    virtual void set_focus();
+    virtual bool remove_focus();
+    virtual bool focused() const;
+    virtual bool focusing() const;
+
+    virtual void update_theme(std::shared_ptr<i_theme> theme_ = nullptr);
+
+    virtual void show();
+    virtual void hide();
+    virtual bool showed() const;
+
+    virtual void enable();
+    virtual void disable();
+    virtual bool enabled() const;
+
+    /// Splitter interface
+    void set_callback(std::function<void(int32_t, int32_t)> callback_);
+
+public:
+    /// Control name in theme
+    static constexpr const char *tc = "splitter";
+
+    /// Used theme values
+    static constexpr const char *tv_calm = "calm";
+    static constexpr const char *tv_active = "active";
+
+private:
+    splitter_orientation orientation;
+    std::function<void(int32_t, int32_t)> callback;
+    std::shared_ptr<i_theme> theme_;
+
+    rect position_;
+
+    std::weak_ptr<window> parent;
+    std::string my_subscriber_id;
+
+    bool showed_, enabled_, active;
+
+    void receive_event(const event &ev);
+
+    void redraw();
+};
+
+}
---
+++ src/control/splitter.cpp
@@ -0,0 +1,224 @@
+//
+// Copyright (c) 2021-2022 Anton Golovkov (udattsk at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/ud84/wui
+//
+
+#include <wui/control/splitter.hpp>
+
+#include <wui/window/window.hpp>
+
+#include <wui/theme/theme.hpp>
+
+#include <wui/system/tools.hpp>
+
+namespace wui
+{
+
+ splitter::splitter(splitter_orientation orientation_, std::function<void(int32_t, int32_t)> callback_, std::shared_ptr<i_theme> theme__)
+    : orientation(orientation_),
+    callback(callback_),
+    theme_(theme__),
+    position_(),
+    parent(),
+    my_subscriber_id(),
+    showed_(true), enabled_(true), active(false)
+{
+}
+
+splitter::~splitter()
+{
+    auto parent_ = parent.lock();
+    if (parent_)
+    {
+        parent_->remove_control(shared_from_this());
+    }
+}
+
+void splitter::draw(graphic &gr, const rect &)
+{
+    if (!showed_)
+    {
+        return;
+    }
+    
+    gr.draw_rect(position(), theme_color(tc, active ? tv_active : tv_calm, theme_));
+}
+
+void splitter::receive_event(const event &ev)
+{
+    if (!showed_ || !enabled_)
+    {
+        return;
+    }
+
+    if (ev.type == event_type::mouse)
+    {
+        switch (ev.mouse_event_.type)
+        {
+            case mouse_event_type::enter:
+            {
+                active = true;
+                auto parent_ = parent.lock();
+                if (parent_)
+                {
+                    if (orientation == splitter_orientation::vertical)
+                    {
+                        set_cursor(parent_->context(), cursor::size_we);
+                    }
+                    else if (orientation == splitter_orientation::horizontal)
+                    {
+                        set_cursor(parent_->context(), cursor::size_ns);
+                    }
+                }
+                redraw();
+
+            }
+            break;
+            case mouse_event_type::leave:
+            {
+                active = false;
+                redraw();
+
+                auto parent_ = parent.lock();
+                if (parent_)
+                {
+                    set_cursor(parent_->context(), cursor::default_);
+                }
+            }
+            break;
+            case mouse_event_type::left_up:
+
+            break;
+        }
+    }
+}
+
+void splitter::set_position(const rect &position__, bool redraw)
+{
+    update_control_position(position_, position__, showed_ && redraw, parent);
+}
+
+rect splitter::position() const
+{
+    return get_control_position(position_, parent);
+}
+
+void splitter::set_parent(std::shared_ptr<window> window_)
+{
+    parent = window_;
+    
+    my_subscriber_id = window_->subscribe(std::bind(&splitter::receive_event, this, std::placeholders::_1), event_type::mouse, shared_from_this());
+}
+
+void splitter::clear_parent()
+{
+    auto parent_ = parent.lock();
+    if (parent_)
+    {
+        parent_->unsubscribe(my_subscriber_id);
+    }
+    parent.reset();
+}
+
+bool splitter::topmost() const
+{
+    return false;
+}
+
+void splitter::set_focus()
+{
+}
+
+bool splitter::remove_focus()
+{
+    return true;
+}
+
+bool splitter::focused() const
+{
+    return false;
+}
+
+bool splitter::focusing() const
+{
+    return false;
+}
+
+void splitter::update_theme(std::shared_ptr<i_theme> theme__)
+{
+    if (theme_ && !theme__)
+    {
+        return;
+    }
+    theme_ = theme__;
+
+    redraw();
+}
+
+void splitter::show()
+{
+    if (!showed_)
+    {
+        showed_ = true;
+        redraw();
+    }
+}
+
+void splitter::hide()
+{
+    if (showed_)
+    {
+        showed_ = false;
+        
+        auto parent_ = parent.lock();
+        if (parent_)
+        {
+            parent_->redraw(position(), true);
+        }
+    }
+}
+
+bool splitter::showed() const
+{
+    return showed_;
+}
+
+void splitter::enable()
+{
+    enabled_ = true;
+    redraw();
+}
+
+void splitter::disable()
+{
+    enabled_ = false;
+    redraw();
+}
+
+bool splitter::enabled() const
+{
+    return enabled_;
+}
+
+void splitter::set_callback(std::function<void(int32_t, int32_t)> callback_)
+{
+    callback = callback_;
+}
+
+void splitter::redraw()
+{
+    if (showed_)
+    {
+        auto parent_ = parent.lock();
+        if (parent_)
+        {
+            parent_->redraw(position());
+        }
+    }
+}
+
+}
