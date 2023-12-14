ea7a1412b5d39bf043fbda17d1cf84c772488b3c
Anton Golovkov <udattsk@gmail.com>
2023-12-14 17:58:18 +0700

    Fixed build on windows

--- include/wui/event/keyboard_event.hpp
+++ include/wui/event/keyboard_event.hpp
@@ -25,21 +25,29 @@ enum class keyboard_event_type
 #ifdef _WIN32
 static const uint8_t vk_tab = 0x09;
 static const uint8_t vk_return = 0x0D;
-static const uint8_t vk_rreturn = 0x13;
+static const uint8_t vk_rreturn = 0;
 static const uint8_t vk_esc = 0x1B;
 
 static const uint8_t vk_back = 0x08;
 static const uint8_t vk_del = 0x2E;
 
 static const uint8_t vk_end = 0x23;
+static const uint8_t vk_nend = 0x61;
 static const uint8_t vk_home = 0x24;
+static const uint8_t vk_nhome = 0x67;
 static const uint8_t vk_page_up = 0x21;
+static const uint8_t vk_npage_up = 0x69;
 static const uint8_t vk_page_down = 0x22;
+static const uint8_t vk_npage_down = 0x63;
 
 static const uint8_t vk_up = 0x26;
+static const uint8_t vk_nup = 0x68;
 static const uint8_t vk_down = 0x28;
+static const uint8_t vk_ndown = 0x62;
 static const uint8_t vk_left = 0x25;
+static const uint8_t vk_nleft = 0x64;
 static const uint8_t vk_right = 0x27;
+static const uint8_t vk_nright = 0x66;
 
 /// The modifier keys
 static const uint8_t vk_capital = 0x14;
