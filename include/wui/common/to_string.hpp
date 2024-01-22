762dfa992fb32642f9cfaa09382d3e5f6edb8404
Anton Golovkov <udattsk@gmail.com>
2024-01-22 11:56:21 +0700

    Added to_string helpers to base types

---
+++ include/wui/common/to_string.hpp
@@ -0,0 +1,23 @@
+//
+// Copyright (c) 2021-2024 Anton Golovkov (udattsk at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/ud84/wui
+//
+
+#pragma once
+
+#include <wui/common/rect.hpp>
+#include <wui/common/color.hpp>
+
+#include <string>
+
+namespace wui
+{
+
+std::string to_string(const rect&);
+std::string to_string(const color_alpha&);
+
+}
---
+++ src/common/to_string.cpp
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2021-2024 Anton Golovkov (udattsk at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/ud84/wui
+//
+
+#include <wui/common/to_string.hpp>
+
+namespace wui
+{
+
+std::string to_string(const rect &r)
+{
+    return "{" + std::to_string(r.left) + ", " + 
+        std::to_string(r.top) + ", " +
+        std::to_string(r.right) + ", " +
+        std::to_string(r.bottom) + "}";
+}
+
+std::string to_string(const color_alpha &ca)
+{
+    return "";
+}
+
+}
