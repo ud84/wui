666b92f6e672370f1a7f9dff55b91cd156738d60
Anton Golovkov <udattsk@gmail.com>
2024-02-10 15:54:55 +0700

    Upd

--- examples/simple/simple.cpp
+++ examples/simple/simple.cpp
@@ -96,27 +96,27 @@ struct PluggedWindow : public std::enable_shared_from_this<PluggedWindow>
 
     PluggedWindow(std::shared_ptr<wui::window> &parentWindow_)
         : parentWindow(parentWindow_),
-        window(new wui::window()),
-        list(new wui::list()),
-        popupMenu(new wui::menu()),
-        panel(new wui::panel()),
-		button1(new wui::button("Button 1", [this]() { 
+        window(std::make_shared<wui::window>()),
+        list(std::make_shared<wui::list>()),
+        popupMenu(std::make_shared<wui::menu>()),
+        panel(std::make_shared<wui::panel>()),
+		button1(std::make_shared<wui::button>("Button 1", [this]() {
             messageBox->show("Lorem Ipsum is simply dummy text of the printing and typesetting industry.\nLorem Ipsum has been the industry's\nstandard dummy text ever since the 1500s, when an unknown printer took\na galley of type and scrambled it to make a type specimen book.",
                 "hello world", wui::message_icon::information, wui::message_button::ok, [](wui::message_result) {});
         }, wui::button_view::image, IMG_ACCOUNT, 16)),
-        button2(new wui::button("Button 2", [this]() {
+        button2(std::make_shared<wui::button>("Button 2", [this]() {
             window->emit_event(310, 200);
         }, wui::button_view::image, IMG_ACCOUNT, 16)),
-        button3(new wui::button("Button 3", [this]() {
-            std::shared_ptr<wui::input> input1(new wui::input());
+        button3(std::make_shared<wui::button>("Button 3", [this]() {
+            std::shared_ptr<wui::input> input1(std::make_shared<wui::input>());
             input1->set_text("98753");
             dialog->add_control(input1, { 10, 35, 200, 60 });
 
-            std::shared_ptr<wui::input> input2(new wui::input());
+            std::shared_ptr<wui::input> input2(std::make_shared<wui::input>());
             input2->set_text("99wegdyug");
             dialog->add_control(input2, { 10, 70, 200, 95 });
 
-            std::shared_ptr<wui::select> select1(new wui::select());
+            std::shared_ptr<wui::select> select1(std::make_shared<wui::select>());
 
             wui::select_items_t items = { { 1, "123" }, { 2, "456" }, { 3, "789" }, { 4, "101112" }, { 5, "131415" }, { 6, "161718" }, { 7, "192021" }, { 8, "222324" } };
 
@@ -128,9 +128,9 @@ struct PluggedWindow : public std::enable_shared_from_this<PluggedWindow>
             
             dialog->init("Modal dialog", { 50, 50, 350, 350 }, wui::window_style::dialog, []() {});
         }, wui::button_view::image, IMG_ACCOUNT, 16)),
-        input(new wui::input("", wui::input_view::password)),
-        messageBox(new wui::message(window, true)),
-        dialog(new wui::window()),
+        input(std::make_shared<wui::input>("", wui::input_view::password)),
+        messageBox(std::make_shared<wui::message>(window, true)),
+        dialog(std::make_shared<wui::window>()),
         creationButton(),
         plugged(false)
     {
@@ -277,13 +277,12 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    std::shared_ptr<wui::window> window(new wui::window());
+    auto window = std::make_shared<wui::window>();
 
-    std::shared_ptr<wui::image> menuImage1(new wui::image(IMG_ACCOUNT));
-    std::shared_ptr<wui::image> menuImage2(new wui::image(IMG_SETTINGS));
+    auto menuImage1 = std::make_shared<wui::image>(IMG_ACCOUNT);
+    auto menuImage2 = std::make_shared<wui::image>(IMG_SETTINGS);
 
-    std::shared_ptr<wui::menu> menu(new wui::menu());
-
+    auto menu = std::make_shared<wui::menu>();
     menu->set_items({
             { 0, wui::menu_item_state::separator, "Bla bla bla", "", menuImage1, {}, [](int32_t i) {} },
             { 1, wui::menu_item_state::normal, "Expand me 1", "", nullptr, {
@@ -305,28 +304,28 @@ int main(int argc, char *argv[])
 
     window->add_control(menu, { 0 });
 
-    std::shared_ptr<wui::button> menuButton(new wui::button("Settings", []() {}, wui::button_view::image, IMG_SETTINGS, 32, wui::button::tc_tool));
+    auto menuButton = std::make_shared<wui::button>("Settings", []() {}, wui::button_view::image, IMG_SETTINGS, 32, wui::button::tc_tool);
     menuButton->set_callback([&menu, &menuButton]() { menu->show_on_control(menuButton, 5); });
     menuButton->disable_focusing();
     window->add_control(menuButton, { 0 });
 
-    std::shared_ptr<wui::progress> horizProgressBar(new wui::progress(0, 100, 50));
+    auto horizProgressBar = std::make_shared<wui::progress>(0, 100, 50);
     window->add_control(horizProgressBar, { 450, 100, 650, 125 });
 
-    std::shared_ptr<wui::slider> horizSlider(new wui::slider(0, 100, 50, [&horizProgressBar](int32_t value) { horizProgressBar->set_value(value); }, wui::slider_orientation::horizontal));
+    auto horizSlider = std::make_shared<wui::slider>(0, 100, 50, [&horizProgressBar](int32_t value) { horizProgressBar->set_value(value); }, wui::slider_orientation::horizontal);
     window->add_control(horizSlider, { 450, 140, 650, 165 });
 
-    std::shared_ptr<wui::progress> vertProgressBar(new wui::progress(0, 100, 80, wui::orientation::vertical));
+    auto vertProgressBar = std::make_shared<wui::progress>(0, 100, 80, wui::orientation::vertical);
     window->add_control(vertProgressBar, { 700, 30, 725, 125 });
 
-    std::shared_ptr<wui::slider> vertSlider(new wui::slider(0, 100, 80, [&vertProgressBar](int32_t value) { vertProgressBar->set_value(value); }, wui::slider_orientation::vertical));
+    auto vertSlider = std::make_shared<wui::slider>(0, 100, 80, [&vertProgressBar](int32_t value) { vertProgressBar->set_value(value); }, wui::slider_orientation::vertical);
     window->add_control(vertSlider, { 660, 30, 685, 155 });
 
-    std::shared_ptr<wui::image> accountImage(new wui::image(IMG_ACCOUNT));
+    auto accountImage = std::make_shared<wui::image>(IMG_ACCOUNT);
     window->add_control(accountImage, { 350, 100, 414, 164 });
 
-    std::shared_ptr<PluggedWindow> pluggedWindow(new PluggedWindow(window));
-    std::shared_ptr<wui::button> createPluggedButton(new wui::button("Create plugged window", []() {}));
+    auto pluggedWindow = std::make_shared<PluggedWindow>(window);
+    auto createPluggedButton = std::make_shared<wui::button>("Create plugged window", []() {});
     createPluggedButton->set_callback([&window, &pluggedWindow, &createPluggedButton]() {
         pluggedWindow.reset();
         pluggedWindow = std::shared_ptr<PluggedWindow>(new PluggedWindow(window));
@@ -346,15 +345,15 @@ int main(int argc, char *argv[])
     }));
     window->add_control(vertSplitter, { 0 });
 
-    std::shared_ptr<wui::text> text0(new wui::text("Lorem Ipsum is simply dummy text of the printing and typesetting industry.\nLorem Ipsum has been the industry's\nstandard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."));
+    auto text0 = std::make_shared<wui::text>("Lorem Ipsum is simply dummy text of the printing and typesetting industry.\nLorem Ipsum has been the industry's\nstandard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
     window->add_control(text0, { 320, 180, 890, 240 });
     
-    std::shared_ptr<wui::input> nameInput(new wui::input(/*"", wui::input_view::password*/));
+    auto nameInput = std::make_shared<wui::input>(/*"", wui::input_view::password*/);
     nameInput->set_text("Hello world!");
     //nameInput->set_input_view(wui::input_view::readonly);
     window->add_control(nameInput, { 320, 250, 890, 275 });
 
-    std::shared_ptr<wui::select> someSelect(new wui::select());
+    auto someSelect = std::make_shared<wui::select>();
     someSelect->set_items({
             { 0, "Item 0" },
             { 1, "Item 1" },
@@ -382,19 +381,19 @@ int main(int argc, char *argv[])
     });
     t.detach();*/
 
-    std::shared_ptr<wui::window> dialog(new wui::window());
+    auto dialog = std::make_shared<wui::window>();
 
-    std::shared_ptr<wui::button> okButton(new wui::button("OK", [window, &dialog]()
+    auto okButton = std::make_shared<wui::button>("OK", [window, &dialog]()
     {
-        std::shared_ptr<wui::input> input1(new wui::input());
+        auto input1 = std::make_shared<wui::input>();
         dialog->add_control(input1, { 10, 35, 200, 60 });
 
-        std::shared_ptr<wui::input> input2(new wui::input());
+        auto input2 = std::make_shared<wui::input>();
         dialog->add_control(input2, { 10, 70, 200, 95 });
 
-        std::shared_ptr<wui::select> select1(new wui::select());
+        auto select1 = std::make_shared<wui::select>();
 
-        std::shared_ptr<wui::list> list1(new wui::list());
+        auto list1 = std::make_shared<wui::list>();
 
         wui::select_items_t items = { { 1, "123" }, { 2, "456" }, { 3, "789" }, { 4, "101112" }, { 5, "131415" }, { 6, "161718" }, { 7, "192021" }, { 8, "222324" } };
 
@@ -404,17 +403,17 @@ int main(int argc, char *argv[])
 
         dialog->add_control(list1, { 10, 245, 200, 400 });
 
-        std::shared_ptr<wui::button> dialogButton(new wui::button("Close", [&dialog]() { dialog->destroy(); }));
-        dialog->add_control(dialogButton, { 10, 410, 100, 435 });
-        dialog->set_default_push_control(dialogButton);
+        auto dialogCloseButton = std::make_shared<wui::button>("Close", [&dialog]() { dialog->destroy(); });
+        dialog->add_control(dialogCloseButton, { 10, 410, 100, 435 });
+        dialog->set_default_push_control(dialogCloseButton);
 
         dialog->set_transient_for(window);
         dialog->init("Modal dialog", { -1, -1, 350, 550 }, wui::window_style::dialog, [&dialog]() { /*dialog.reset();*/ });
-    }));
+    });
 
-    std::shared_ptr<wui::button> cancelButton(new wui::button("Cancel", [window]() { window->destroy(); }, wui::button_view::image_right_text, IMG_ACCOUNT, 24, wui::button::tc, MakeRedButtonTheme()));
+    auto cancelButton = std::make_shared<wui::button>("Cancel", [window]() { window->destroy(); }, wui::button_view::image_right_text, IMG_ACCOUNT, 24, wui::button::tc, MakeRedButtonTheme());
 
-    std::shared_ptr<wui::button> darkThemeButton(new wui::button("Set the dark theme",
+    auto darkThemeButton = std::make_shared<wui::button>("Set the dark theme",
         [&window, &pluggedWindow, &dialog, &cancelButton]() {
         auto current_theme = "dark";
         wui::set_current_app_theme(current_theme);
@@ -430,12 +429,12 @@ int main(int argc, char *argv[])
         pluggedWindow->window->update_theme();
         dialog->update_theme(); 
         cancelButton->update_theme(MakeRedButtonTheme());
-    }));
+    });
     window->add_control(darkThemeButton, { 320, 350, 440, 375 });
 
     darkThemeButton->turn(true);
 	
-    std::shared_ptr<wui::button> whiteThemeButton(new wui::button("Set the light theme", 
+    auto whiteThemeButton = std::make_shared<wui::button>("Set the light theme",
         [&window, &pluggedWindow, &dialog, &cancelButton]() {
         auto current_theme = "light";
         wui::set_current_app_theme(current_theme);
@@ -451,7 +450,7 @@ int main(int argc, char *argv[])
         pluggedWindow->window->update_theme();
         dialog->update_theme();
         cancelButton->update_theme(MakeRedButtonTheme());
-    }));
+    });
     window->add_control(whiteThemeButton, { 500, 350, 620, 375 });
 
     window->add_control(okButton, { 240, 450, 350, 480 });
