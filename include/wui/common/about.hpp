21a8d1f6da8068ce0998917d57215d900e3cb064
Anton Golovkov <udattsk@gmail.com>
2024-02-10 19:22:57 +0700

    Version inc

--- include/wui/common/about.hpp
+++ include/wui/common/about.hpp
@@ -17,7 +17,7 @@ namespace about
 
 static constexpr const char * name = "libWUI";
 static constexpr const char * full_name = "Window User Interface Library";
-static constexpr const char * version = "1.0.240206";
+static constexpr const char * version = "1.1.240210";
 static constexpr const char * web = "https://libwui.org";
 static constexpr const char * authors = "IVS LLC, Anton Golovkov";
 static constexpr const char * official_repo = "https://github.com/ud84/wui";
