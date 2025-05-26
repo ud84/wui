ead49aee190a523c1865977bd61a1d749a814884
Anton Golovkov <udattsk@gmail.com>
2025-05-26 09:07:48 +0300

    Added redhat deps sh

---
+++ rh_deps.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo dnf install cairo-devel
+sudo dnf install xcb-util-image
+sudo dnf install xcb-util-devel
+sudo dnf install xcb-util-image-devel
+sudo dnf install xcb-util-cursor-devel
+sudo dnf install libudev-devel
+sudo dnf install xcb-util-wm-devel
+sudo dnf install libstdc++-devel
+sudo dnf install libstdc++-static
