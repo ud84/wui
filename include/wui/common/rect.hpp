6c3bacb26188e99e35e626e0606a44b900f619ae
Serj Komarinsky <slkom2@gmail.com>
2026-06-14 00:34:40 +0300

    feat: new methods added

--- include/wui/common/rect.hpp
+++ include/wui/common/rect.hpp
@@ -9,6 +9,14 @@
 #pragma once
 
 #include <cstdint>
+#include <algorithm>
+
+#ifdef min
+#   undef min
+#endif
+#ifdef max
+#   undef max
+#endif
 
 namespace wui
 {
@@ -17,55 +25,80 @@ struct rect
 {
     int32_t left, top, right, bottom;
 
-    inline bool operator==(rect lv)
+    inline bool operator==(const rect &lr) noexcept
     {
-        return left == lv.left && top == lv.top && right == lv.right && bottom == lv.bottom;
+        return lr.left == left && lr.top == top && lr.right == right && lr.bottom == bottom;
     }
 
-    inline bool operator>(rect lv)
+    inline bool operator>(const rect &lr) noexcept
     {
-        return width() > lv.width() && height() > lv.height();
+        return width() > lr.width() && height() > lr.height();
     }
 
-    inline rect operator+(rect lv)
+    inline rect operator+(const rect &lr) noexcept
     {
-        return rect{ left + lv.left, top + lv.top, left + lv.right, top + lv.bottom };
+        // что делает? геом. смысл
+        return rect{ left + lr.left, top + lr.top, left + lr.right, top + lr.bottom };
+
+        // может так?
+        //rect r;
+        //max(r, *this, lr);
+        //return r;
     }
 
-    inline bool in(int32_t x, int32_t y) const
+    inline void clear() noexcept
+    {
+        *this = rect{};
+    }
+
+    inline bool in(const int32_t x, const int32_t y) const noexcept
     {
         return x >= left && x <= right && y >= top && y <= bottom;
     }
 
-    inline bool in(rect outer) const
+    inline bool in(const rect &outer) const noexcept
     {
         return !((outer.right <= left || right <= outer.left || outer.bottom <= top || bottom <= outer.top));
     }
 
-    inline bool is_null() const
+    inline bool is_null() const noexcept
     {
-        return left == 0 && top == 0 && right == 0 && bottom == 0;
+        return 0 == left && 0 == top && 0 == right && 0 == bottom;
     }
 
-    inline int32_t width() const
+    inline bool empty() const noexcept
+    {
+        return right == left || bottom == top;
+    }
+
+    inline int32_t width() const noexcept
     {
         return right - left;
     }
 
-    inline int32_t height() const
+    inline int32_t height() const noexcept
     {
         return bottom - top;
     }
 
-    inline void move(int32_t x, int32_t y)
+    inline void move(const int32_t x, const int32_t y) noexcept
     {
         left += x;
         top += y;
         right += x;
         bottom += y;
     }
+    inline void set(const int32_t left_, const int32_t top_) noexcept
+    {
+        int32_t t = right - left;
+        left = left_;
+        right = left_ + t;
+        t = bottom - top;
+        top = top_;
+        bottom = top_ + t;
+    }
 
-    inline void put(int32_t x, int32_t y)
+    inline void put(const int32_t x, const int32_t y) noexcept
     {
         right = x + width();
         bottom = y + height();
@@ -73,12 +106,55 @@ struct rect
         top = y;
     }
 
-    inline void widen(int32_t val)
+    inline void resize(const int32_t width_, const int32_t height_) noexcept
+    {
+        right = left + width_;
+        bottom = top + height_;
+    }
+
+    inline void widen(const int32_t val) noexcept
     {
         left -= val;
         top -= val;
         right += val;
         bottom += val;
+    }
+
+    inline void widen(const int32_t dx, const int32_t dy) noexcept
+    {
+        left -= dx;
+        top -= dx;
+        right += dy;
+        bottom += dy;
+    }
+
+    static inline rect min(const rect& r1, const rect& r2) noexcept
+    {
+        rect r;
+        min(r, r1, r2);
+        return r;
+    }
+
+    static inline void min(rect& r_out, const rect& r1, const rect& r2) noexcept
+    {
+        r_out.left = std::max(r1.left, r2.left);
+        r_out.right = std::min(r1.right, r2.right);
+        r_out.top = std::max(r1.top, r2.top);
+        r_out.bottom = std::min(r1.bottom, r2.bottom);
+    }
+
+    static inline rect max(const rect &r1, const rect& r2) noexcept
+    {
+        rect r;
+        max(r, r1, r2);
+        return r;
+    }
+    static inline void max(rect &r_out, const rect &r1, const rect& r2) noexcept
+    {
+        r_out.left = std::min(r1.left, r2.left);
+        r_out.right = std::max(r1.right, r2.right);
+        r_out.top = std::min(r1.top, r2.top);
+        r_out.bottom = std::max(r1.bottom, r2.bottom);
     }
 };
 
