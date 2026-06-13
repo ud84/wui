5183a33f80a492f4845ac7536b6c5e4f5af779ae
Serj Komarinsky <slkom2@gmail.com>
2026-06-14 00:27:00 +0300

    Added `constexpr`

--- include/wui/common/bit_helpers.hpp
+++ include/wui/common/bit_helpers.hpp
@@ -12,31 +12,31 @@ namespace wui
 {
 
 template <typename T, typename U>
-inline void set_bit(T &reg, U bit)
+constexpr inline void set_bit(T &reg, U bit)
 {
     reg |= (1 << static_cast<int>(bit));
 }
 
 template <typename T, typename U>
-inline void clear_bit(T &reg, U bit)
+constexpr inline void clear_bit(T &reg, U bit)
 {
     reg &= (~(1 << static_cast<int>(bit)));
 }
 
 template <typename T, typename U>
-inline void inv_bit(T &reg, U bit)
+constexpr inline void inv_bit(T &reg, U bit)
 {
     reg ^= (1 << static_cast<int>(bit));
 }
 
 template <typename T, typename U>
-inline bool bit_is_set(T reg, U bit)
+constexpr inline bool bit_is_set(T reg, U bit)
 {
     return ((static_cast<int>(reg) & (1 << static_cast<int>(bit))) != 0);
 }
 
 template <typename T, typename U>
-inline bool bit_is_clear(T reg, U bit)
+constexpr inline bool bit_is_clear(T reg, U bit)
 {
     return ((static_cast<int>(reg) & (1 << static_cast<int>(bit))) == 0);
 }
